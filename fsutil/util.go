126023140da9348a50f10eec2cf408133448c769
Fan Yang <me@minidump.info>
2018-01-27 08:52:12 +0800

    (+)add fsutil

---
+++ fsutil/util.go
@@ -0,0 +1,21 @@
+package fsutil
+
+import (
+	"errors"
+	"os"
+)
+
+// FileExists check if the given file exists
+func FileExists(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if err == nil {
+		if stat.Mode()&os.ModeType == 0 {
+			return true, nil
+		}
+		return false, errors.New(path + " exists but is not regular file")
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
