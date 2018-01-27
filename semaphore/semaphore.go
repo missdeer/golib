1729e3235679ebcfcb52be72c5909855cbec1b07
Fan Yang <me@minidump.info>
2018-01-27 09:19:14 +0800

    (*)rename

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -9,34 +9,34 @@ import (
 
 // Semaphore represent a semaphore object
 type Semaphore struct {
-	s   *sema.Weighted
+	w   *sema.Weighted
 	ctx context.Context
 }
 
 // New create a semaphore object
 func New(n int) *Semaphore {
 	return &Semaphore{
-		s:   sema.NewWeighted(int64(n)),
+		w:   sema.NewWeighted(int64(n)),
 		ctx: context.TODO(),
 	}
 }
 
 // Acquire reference increased
 func (s *Semaphore) Acquire() {
-	s.s.Acquire(s.ctx, 1)
+	s.w.Acquire(s.ctx, 1)
 }
 
 // Release reference decreased
 func (s *Semaphore) Release() {
-	s.s.Release(1)
+	s.w.Release(1)
 }
 
 // AcquireN reference increased
 func (s *Semaphore) AcquireN(n int64) {
-	s.s.Acquire(s.ctx, n)
+	s.w.Acquire(s.ctx, n)
 }
 
 // ReleaseN reference decreased
 func (s *Semaphore) ReleaseN(n int64) {
-	s.s.Release(n)
+	s.w.Release(n)
 }
